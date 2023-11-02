a547d1e6281813f1c0ee19f39fc3e7386e7c572b
Julipoli99 <Polimenijulian9@gmail.com>
2023-11-01 22:09:37 -0300

    Nuevo diseño en el detalle de pelicula con sus respectivos datos

--- Movie-PNT2/src/components/MovieDetail.vue
+++ Movie-PNT2/src/components/MovieDetail.vue
@@ -1,6 +1,29 @@
 <template>
-    
-    
+    <body>
+    <div class="contenedor">
+        <header>
+            <h1>{{ movie.original_title }}</h1>
+            <p class="anio">{{ movie.release_date }}</p>
+        </header>
+        <div class="detalles">
+            <div class="portada">
+                <img :src="imageFilter(movie.backdrop_path)" alt="Título de la Película" width="800" height="600">
+            </div>
+            <div class="informacion">
+                <p><strong>Director:</strong> {{ director.name }}</p>
+               
+                <p><strong>Reparto:</strong></p>
+                <ul>
+                  <li v-for="actor in actors">{{ actor }}</li>
+                </ul>
+                <p class="sinopsis"><strong>Sinopsis:</strong> {{ movie.overview }}</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+    <!--
   <div class="contenedor">
     <img :src="imageFilter(movie.backdrop_path)" alt="">
     <div class="details">
@@ -8,17 +31,22 @@
     <h3>Overview</h3>
     <p>{{ movie.overview }}</p>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
-import { defineProps, onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import MovieService from '../services/MovieService';
 
 const route = useRoute()
 const service = new MovieService()
 const movie = service.getMovie();
+const credits = service.getCredits();
+const director = service.getDirector();
+const actors = service.getActors();
+
+const threeActors = ref([])
 
 
 
@@ -27,6 +55,7 @@ const movie = service.getMovie();
 onMounted(async() => {
     const id = route.params.id
     await service.getMovieById(id)
+    await service.getCreditsOfMovie(id)
 })
 
 const imageFilter = (image) => "https://image.tmdb.org/t/p/w300/" + image
@@ -36,6 +65,69 @@ const imageFilter = (image) => "https://image.tmdb.org/t/p/w300/" + image
 
 <style scoped>
 
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f5f5f5;
+    margin: 0;
+    padding: 0;
+}
+
+.contenedor {
+    max-width: 950px;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    text-align: left;
+}
+
+header {
+    text-align: center;
+}
+
+h1 {
+    font-size: 32px;
+    margin: 0;
+    color: #333;
+}
+
+.anio {
+    font-size: 16px;
+    margin: 5px 0;
+    color: #666;
+}
+
+.detalles {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+}
+
+.portada img {
+    max-width: 100%;
+    height: auto;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.informacion {
+    flex-grow: 1;
+    padding: 0 20px;
+}
+
+p {
+    font-size: 18px;
+    margin: 10px 0;
+    color: #666;
+}
+
+.sinopsis {
+    font-size: 16px;
+}
+
+
+/*
 .contenedor{
     outline: 3px solid red;
     background-color: lightblue;
@@ -56,6 +148,6 @@ img{
     width: 20vw;
     height: 50vh;
 }
-
+*/
 
 </style>
--- Movie-PNT2/src/services/MovieService.js
+++ Movie-PNT2/src/services/MovieService.js
@@ -3,10 +3,16 @@ import { ref } from "vue";
 class MovieService {
     movies;
     movie;
+    credits;
+    director;
+    actors;
 
     constructor(){
         this.movies = ref([])
         this.movie = ref("")
+        this.credits = ref("")
+        this.director = ref("");
+        this.actors = ref([])
     }
 
     getMovies(){
@@ -17,16 +23,28 @@ class MovieService {
         return this.movie
     }
 
+    getCredits(){
+        return this.credits
+    }
+
+    getDirector(){
+        return this.director
+    }
+
+    getActors(){
+        return this.actors;
+    }
+
     async fetchPopularMovies(){
         try {
             const url = "https://api.themoviedb.org/3/movie/popular?api_key=b2dd2751da85c7ad32671ddb27a345d1";
             const response = await fetch(url);
-            console.log(response);
+          //  console.log(response);
             const json = await response.json()
             const data = await json.results;
             this.movies.value = data
 
-            console.log(this.movies.value)
+        //    console.log(this.movies.value)
 
         } catch (error) {
             console.error(error);
@@ -37,12 +55,36 @@ class MovieService {
         try {
             const url = `https://api.themoviedb.org/3/movie/${id}?api_key=b2dd2751da85c7ad32671ddb27a345d1`;
             const response = await fetch(url)
-            console.log(response);
+           // console.log(response);
             const json = await response.json()
-            console.log(json);
+          //  console.log(json);
             this.movie.value = await json
 
            // console.log(this.movie.value)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async getCreditsOfMovie(idMovie){
+        try {
+            const url = `https://api.themoviedb.org/3/movie/${idMovie}/credits?api_key=b2dd2751da85c7ad32671ddb27a345d1`
+            const response = await fetch(url)
+            console.log(response);
+            const json = await response.json()
+            console.log(json);
+            this.credits.value = await json
+
+            const crew = json.crew.filter((departament) => departament.known_for_department == 'Directing' && departament.department == 'Writing' || departament.job == 'Director')
+
+            this.director.value = { ...crew[0]}
+           // console.log(json.crew.filter((departament) => departament.known_for_department == 'Directing' && departament.department == 'Writing'));
+            for(let i = 0; i < 3; i++){
+                this.actors.value.push(json.cast[i].name)
+            }
+
+            //console.log(this.actors.value);
+        
         } catch (error) {
             console.error(error)
         }
